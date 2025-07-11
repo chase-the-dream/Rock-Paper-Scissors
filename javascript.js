48c0a9b829db4e3d8c043efdebdf3f4043051d5b
JuiceChamp <freckmannchase@gmail.com>
2025-07-11 19:02:05 -0400

    Project rough completion

--- javascript.js
+++ javascript.js
@@ -1,13 +1,73 @@
-const choice = document.querySelector(".choice-wrap")
+const choices = document.querySelectorAll(".choice-wrap");
 const result = document.querySelector(".results");
 
-function computerChoice() {
+function getComputerChoice() {
     return Math.floor(Math.random() * 3);
+}
+// 0 - Rock / 1 - Paper / 2 - Scissors
+function determineWinner(userChoice, computerChoice)
+{
+    if(userChoice == 'rock')
+    {
+        if(computerChoice == 1)
+        {
+            return 'loss';
+        }
+        else if(computerChoice == 2)
+        {
+            return 'win';
+        }
+    }
+    if(userChoice == 'paper')
+    {
+        if(computerChoice == 2)
+        {
+            return 'loss';
+        }
+        else if(computerChoice == 0)
+        {
+            return 'win';
+        }
+    }
+    if(userChoice == 'scissors')
+    {
+        if(computerChoice == 1)
+        {
+            return 'win';
+        }
+        else if(computerChoice == 0)
+        {
+            return 'loss';
+        }
+    }
+    return 'tie';
+}
+
+function updateResultBox(gameResult)
+{
+    if(gameResult == 'win')
+    {
+        return 'Congratulations you win!'
+    }
+    else if(gameResult == 'loss')
+    {
+        return 'The Computer has won'
+    }
+    else
+    {
+        return 'It was a tie!'
+    }
 }
 
 function playGame(event) {
-    console.log(event);
-}
+    const choiceElement = event.target.closest('.choice-wrap');
+    const userChoice = choiceElement.dataset.choice;
+    const computerChoice = getComputerChoice();
 
-choice.addEventListener("click", playGame)
+    const matchResult = determineWinner(userChoice, computerChoice);
+    result.textContent = updateResultBox(matchResult);
+} 
 
+choices.forEach(choice => {
+    choice.addEventListener("click", playGame);
+})
