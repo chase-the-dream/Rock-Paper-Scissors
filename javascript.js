29969e2af2e3d7a95800279d67dd113bac16cc97
Chase <ch517661@ucf.edu>
2025-07-11 13:35:50 -0400

    started js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,13 @@
-let rock = document.querySelector("#rock")
-let paper = document.querySelector("#paper")
-let scissors = document.querySelector("#scissors")
+const choice = document.querySelector(".choice-wrap")
+const result = document.querySelector(".results");
+
+function computerChoice() {
+    return Math.floor(Math.random() * 3);
+}
+
+function playGame(event) {
+    console.log(event);
+}
+
+choice.addEventListener("click", playGame)
 
